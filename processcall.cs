c9013a1c077c3397654a8f5357213a0d80f71d10
Susmitha Gankidi <susmitha.gankidi@ggktech.local>
2017-06-28 11:44:39 +0530

    these are programs for static proof

---
+++ processcall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Diagnostics;
+
+namespace statictestprocesscall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Process.Start("C:/Users/susmitha.gankidi/sushcodes/code35/samplecode/obj/Debug/NET451/samplecode.exe");
+           Console.ReadLine();
+        }
+    }
+}
---
+++ sampleprogramcalled.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace samplecode
+{
+    class Program
+    {
+         int i = 20;
+        static void Main(string[] args)
+        {
+            i++;
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
