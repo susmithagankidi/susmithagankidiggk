0a0047de58c39fc5a85b50d4d0dbcf4b09ee222e
Susmitha Gankidi <susmitha.gankidi@ggktech.local>
2017-07-01 15:34:28 +0530

    codes

---
+++ codes/process.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace processcall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("C:/Users/susmitha.gankidi/sushcodes/code43/code43subproject1/bin/Debu/NET451/code43subproject1.exe");
+            Console.ReadLine();
+        }
+    }
+}
