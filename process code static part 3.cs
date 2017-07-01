54156e3b0b9914a762f50f16aaf97c1bc8077251
Susmitha Gankidi <susmitha.gankidi@ggktech.local>
2017-07-01 14:24:08 +0530

    codes

---
+++ process code static part 3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Processcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start("C:/Users/susmitha.gankidi/sushcodes/code42/samplecode/bin/Debug/NET451/samplecode.exe");
+            
+        }
+    }
+}
