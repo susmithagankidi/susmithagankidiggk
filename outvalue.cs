067151a08c7dd2a1b73e67d36f938eea33b21c9d
Susmitha Gankidi <susmitha.gankidi@ggktech.local>
2017-07-01 17:28:22 +0530

    this is out proof

---
+++ outvalue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace passingparameters
+{
+    class Program
+    {
+        static void somefunction(int j,out int i)
+        {
+         i=20;
+         j=100;
+        }
+        static void Main(string[] args)
+        {
+            int i =0;
+            int j = 20;
+            // Displaying the original values
+            Console.WriteLine("The original value if i "+i);
+            Console.WriteLine("The original value of j "+j);
+           // calling the method somefunction
+           somefunction(j,out i);
+           Console.WriteLine("i= "+i);
+           Console.WriteLine("j = " +j);
+        }
+    }
+}
