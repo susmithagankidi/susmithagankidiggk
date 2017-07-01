e22e69e688f8241abdd71c239ea31ee03ebac96c
Susmitha Gankidi <susmitha.gankidi@ggktech.local>
2017-07-01 14:10:57 +0530

    these are codes for static proof

---
+++ static program part1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace samplecode
+{
+    class Program
+    {
+        public static int i = 20;
+        static void Main(string[] args)
+        {
+            i++;
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
+
