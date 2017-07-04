b2e0fd7ef276ccce85639b7275cf86807e51e32a
Susmitha Gankidi <susmitha.gankidi@ggktech.local>
2017-07-04 09:47:25 +0530

    ndigitaddition

--- ndigitaddition/methodsforadding.cs
+++ ndigitaddition/methodsforadding.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
@@ -21,7 +21,7 @@ namespace assignment1
          Console.WriteLine(term1);
          return term1; }
        }
-
+        // method for adding numbers (strings here)
        public string sumcalculation(string term1,string term2)
        {
            int carry = 0;
@@ -37,6 +37,7 @@ namespace assignment1
            Console.WriteLine(res);
            return res;
        }
+       // method for reversing the string
        public string reversing(string res1)
        {
          string reverse="";
@@ -51,44 +52,5 @@ namespace assignment1
        }
 
 
-    }
-
-    class Program
-     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter term 1");
-            string term1 = Console.ReadLine();
-            Console.WriteLine("Enter term 2");
-            string term2 = Console.ReadLine();
-            int n1 = term1.Length;
-            int n2 = term2.Length;
-            Console.WriteLine("term 1 length is {0}",n1);
-            Console.WriteLine("term 2 length is {0}",n2);
-           // Creating an object for class Methods 
-           Methods p = new Methods();
-           // Calling the method padding
-           string x= p.padding(term1,term2);
-           if(n1>n2)
-           {
-              term2 = x;
-           } 
-           else
-           {
-               term1 = x;
-           }
-           
-           Console.WriteLine("after padding"+term1);
-           Console.WriteLine("after padding"+term2);
-           // calling the method for calculating sum
-            string res1=p.sumcalculation(term1,term2);
-            Console.WriteLine("the added value in the string");
-            Console.WriteLine(res1);
-            // calling the method for reversing the string
-            string res2 = p.reversing (res1);
-            Console.WriteLine(res2);
-
-
-        }
     }
 }
